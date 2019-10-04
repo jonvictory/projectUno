83aead16b9de0aca154f5e7e3e2cf7287e9ca67b
jonhuynh <jonvictory@gmail.com>
2019-10-03 21:04:57 -0600

    added basic map functions, connected to the HTML, have functions in place for markers and city search, but need more development for those to work

--- public/assets/javascript/project.js
+++ public/assets/javascript/project.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-
+    initMap();
     //variables for Yelp API calls
     var term = '';
     var location = '';
-
+    
 
     //search onclick that grabs values and stores from term and location
     $("#search").on("click", function (event) {
         location = $("#locationInput").val().trim();
         term = $("#termInput").val().trim();
         yelpAPI();
+        searchMap()
         console.log(location);
         console.log(term);
     });
@@ -38,5 +39,27 @@ $(document).ready(function () {
 
     }
 
-    //End Document Ready
+    //google map API js
+    
+
+    var map
+    var slc = {lat: 40.7608, lng: -111.8910};
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.7608, lng: -111.8910},
+        zoom: 8
+      });
+    }
+
+    function searchMap() {
+        // The location of Uluru
+        var searchLocation = {lat: 41.7608, lng: -115.8910}
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 4, center: slc});
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({position: searchLocation, map: map});
+      }
+    
+   
 });
